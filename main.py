c972974df4e212665698a9839052b8ecd3b9369e
unknown <tdellard1@my.stlcc.edu>
2017-10-04 01:31:43 -0500

    Most of project finished

--- main.py
+++ main.py
@@ -7,7 +7,63 @@ app.config['DEBUG'] = True
 
 @app.route('/user-signup')
 def usersignup():
-    return render_template("user_sign_up.html", title='User Sign Up')
+    username = request.args.get('user')
+    email = request.args.get('email')
+    return render_template("user_sign_up.html", title='User Sign Up', username=username, email=email)
+
+def is_valid(text):
+    error = ''
+    if len(text) >= 3 and len(text) <= 20:
+        for char in text:
+            if char == "":
+                error = 'should contain no space.'
+                break
+                return False
+            else:
+                return True
+    else:
+        error = 'is not correct length.'
+        return False
+    
+def is_empty(text):
+    if text == '':
+        return True 
+    else:
+        return False
+
+def password_match(text, text2):
+    error = ''
+    if text == text2:
+        return True
+    else:
+        error = 'Passwords do not match.'
+        return False
+
+def is_email(text):
+    if text != '':
+        error = ''
+        if is_valid(text):
+            email = ''
+            for char in text:
+                if char == '@':
+                    email += '@'
+                else:
+                    pass
+                if char == '.':
+                    email += '.'
+                else:
+                    pass
+            if email == '@.':
+                return True
+            else:
+                error = ' is not a valid email.'
+                return False
+
+        else:
+            return False    
+    else: 
+        return True
+
 
 @app.route('/user-validate', methods=['POST'])
 def user_validate():
@@ -15,35 +71,41 @@ def user_validate():
     password = request.form['password']
     vpassword = request.form['vpassword']
     email = request.form['email']
+    proceed = False
+    error = ''
 
-    empty_field = True
+    forms = (username, password, vpassword)
 
-    if username == '': 
-        empty_field = True 
-    elif password == '': 
-        empty_field = True
-    elif vpassword == '': 
-        empty_field = True
-    elif email == '': 
-        empty_field = True
+    if is_empty(username) or is_empty(password) or is_empty(vpassword):
+        proceed = False
     else:
-        empty_field = False
+        if is_valid(username) and is_valid(password):
+            if password_match(password, vpassword):
+                if is_email(email):
+                    proceed = True
+                else:
+                    proceed = False
+            else:
+                proceed = False
+        else: 
+            proceed = False
 
-    if empty_field == True:
-        return redirect('/user-signup')
-    elif empty_field == False:
-        return redirect('/user-confirmed')
+
+
+    if proceed == False:
+        return redirect('/user-signup?user={0}&email={1}&error={2}'.format(username, email, error))
     else:
-        pass
+        return redirect('/user-confirmed?user={0}'.format(username))
+        
+        
+@app.route('/re-user-signup')
+def reusersignup():
+    username = request.args.get('user')
+    email = request.args.get('email')
+    return render_template("user_sign_up.html", title='User Sign Up', username=username, email=email)
 
-
-
-
-@app.route('/user-confirmed', methods=['get'])
+@app.route('/user-confirmed')
 def user_confirmed():
-    username = request.form['username']
-    return render_template("user_confirmed.html", user=username)
-
-
-
+    username = request.args.get('user') 
+    return render_template("user_confirmed.html", user = username)
 app.run()
